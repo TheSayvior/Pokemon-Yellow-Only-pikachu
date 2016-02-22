1db9e5349c4d76802055a2b2b49832811338a9a4
AAwezz <anders_wess@hotmail.com>
2016-02-22 12:00:47 +0100

    Eyetracking 1.0

---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseDown()
+    {
+        GetComponent<Renderer>().material.color = Color.black;
+    }
+}
