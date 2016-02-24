ace7a674f255e05069a45fa3aef07756fc03a8e6
AAwezz <anders_wess@hotmail.com>
2016-02-24 15:12:09 +0100

    Set lightning to 0.05 overall. Added first room to the left as test. Deleted everything about footsteps and jump sounds. Added classroom props. Changed every shader to Diffuse.

--- Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -87,7 +87,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
         private void PlayLandingSound()
         {
             m_AudioSource.clip = m_LandSound;
-            m_AudioSource.Play();
+            //m_AudioSource.Play();
             m_NextStep = m_StepCycle + .5f;
         }
 
@@ -116,7 +116,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
                 if (m_Jump)
                 {
                     m_MoveDir.y = m_JumpSpeed;
-                    PlayJumpSound();
+                    //PlayJumpSound();
                     m_Jump = false;
                     m_Jumping = true;
                 }
@@ -127,54 +127,54 @@ namespace UnityStandardAssets.Characters.FirstPerson
             }
             m_CollisionFlags = m_CharacterController.Move(m_MoveDir*Time.fixedDeltaTime);
 
-            ProgressStepCycle(speed);
+            //ProgressStepCycle(speed);
             UpdateCameraPosition(speed);
 
             m_MouseLook.UpdateCursorLock();
         }
 
 
-        private void PlayJumpSound()
-        {
-            m_AudioSource.clip = m_JumpSound;
-            m_AudioSource.Play();
-        }
+        //private void PlayJumpSound()
+        //{
+        //    m_AudioSource.clip = m_JumpSound;
+        //    m_AudioSource.Play();
+        //}
 
 
-        private void ProgressStepCycle(float speed)
-        {
-            if (m_CharacterController.velocity.sqrMagnitude > 0 && (m_Input.x != 0 || m_Input.y != 0))
-            {
-                m_StepCycle += (m_CharacterController.velocity.magnitude + (speed*(m_IsWalking ? 1f : m_RunstepLenghten)))*
-                             Time.fixedDeltaTime;
-            }
+        //private void ProgressStepCycle(float speed)
+        //{
+        //    if (m_CharacterController.velocity.sqrMagnitude > 0 && (m_Input.x != 0 || m_Input.y != 0))
+        //    {
+        //        m_StepCycle += (m_CharacterController.velocity.magnitude + (speed*(m_IsWalking ? 1f : m_RunstepLenghten)))*
+        //                     Time.fixedDeltaTime;
+        //    }
 
-            if (!(m_StepCycle > m_NextStep))
-            {
-                return;
-            }
+        //    if (!(m_StepCycle > m_NextStep))
+        //    {
+        //        return;
+        //    }
 
-            m_NextStep = m_StepCycle + m_StepInterval;
+        //    m_NextStep = m_StepCycle + m_StepInterval;
 
-            PlayFootStepAudio();
-        }
+        //    PlayFootStepAudio();
+        //}
 
 
-        private void PlayFootStepAudio()
-        {
-            if (!m_CharacterController.isGrounded)
-            {
-                return;
-            }
-            // pick & play a random footstep sound from the array,
-            // excluding sound at index 0
-            int n = Random.Range(1, m_FootstepSounds.Length);
-            m_AudioSource.clip = m_FootstepSounds[n];
-            m_AudioSource.PlayOneShot(m_AudioSource.clip);
-            // move picked sound to index 0 so it's not picked next time
-            m_FootstepSounds[n] = m_FootstepSounds[0];
-            m_FootstepSounds[0] = m_AudioSource.clip;
-        }
+        //private void PlayFootStepAudio()
+        //{
+        //    if (!m_CharacterController.isGrounded)
+        //    {
+        //        return;
+        //    }
+        //    // pick & play a random footstep sound from the array,
+        //    // excluding sound at index 0
+        //    int n = Random.Range(1, m_FootstepSounds.Length);
+        //    m_AudioSource.clip = m_FootstepSounds[n];
+        //    m_AudioSource.PlayOneShot(m_AudioSource.clip);
+        //    // move picked sound to index 0 so it's not picked next time
+        //    m_FootstepSounds[n] = m_FootstepSounds[0];
+        //    m_FootstepSounds[0] = m_AudioSource.clip;
+        //}
 
 
         private void UpdateCameraPosition(float speed)
